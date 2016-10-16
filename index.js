36c6d8a5a8df5fca7478d854b864e34e0f26320f
Michael Hogg <michael.hogg.uk@gmail.com>
2016-10-16 13:24:52 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,140 @@
+'use strict';
+
+const chalk        = require('chalk');
+const childProcess = require('child_process');
+const flow         = require('flow-bin');
+
+
+/**
+ * Format a Blame message object.
+ *
+ * @param  {Object} msg
+ * @return {Array<string>}
+ */
+function formatBlameMessage(msg) {
+
+    let lines = [
+        chalk.cyan(msg.path + ':' + msg.line),
+        chalk.yellow(msg.context)
+    ];
+
+    const indent = ' '.repeat(msg.start - 1);
+    const type   = '<' + msg.descr + '>';
+
+    if (msg.endline === msg.line) {
+        // Single line of source code
+        const marker = '^'.repeat((msg.end - msg.start) + 1);
+        lines.push(indent + chalk.red(marker + ' ' + type));
+    } else {
+        // Multiple lines of source code
+        lines.push(indent + chalk.red('^ ' + type));
+    }
+
+    return lines;
+
+}
+
+/**
+ * Format a Comment message object.
+ *
+ * @param  {Object} msg
+ * @return {Array<string>}
+ */
+function formatCommentMessage(msg) {
+
+    const lines = [
+        chalk.red(msg.descr)
+    ];
+
+    return lines;
+
+}
+
+/**
+ * Format a Flow error object.
+ *
+ * @param  {Object} err
+ * @return {string}
+ */
+function formatFlowErrorObject(err) {
+
+    const formattedLines = err.message.map(function (msg) {
+        let lines = [];
+        switch (msg.type) {
+            case 'Blame':
+                lines = formatBlameMessage(msg);
+                break;
+            case 'Comment':
+                lines = formatCommentMessage(msg);
+                break;
+            default:
+                throw new Error('Unrecognised error message: ' + msg);
+        }
+        return lines.map(line => '    ' + line).join('\n');
+    });
+
+    return formattedLines.join('\n\n');
+
+}
+
+/**
+ * Format errors reported by Flow.
+ *
+ * @param  {string} lintPath   - Path of the source file being linted
+ * @param  {string} flowStdout - STDOUT from Flow
+ * @return {string}
+ */
+function formatErrors(lintPath, flowStdout) {
+
+    let formattedErrors = [];
+
+    JSON.parse(flowStdout).errors.forEach(function (error) {
+        const pathMatch = error.message.some(function (msg) {
+            return (msg.type === 'Blame' && lintPath === msg.path);
+        });
+        if (pathMatch) {
+            formattedErrors.push(formatFlowErrorObject(error));
+        }
+    });
+
+    return formattedErrors.join('\n\n' + '-'.repeat(20) + '\n\n');
+
+}
+
+/**
+ * @see brunch.io/docs/plugins#boilerplate-plugin
+ * @see github.com/flowtype/flow-bin#api
+ */
+class FlowLinter {
+
+    constructor(brunchConfig) {
+        this.config = (brunchConfig && brunchConfig.plugins && brunchConfig.plugins.flowtype) || {};
+        try {
+            // Start Flow server (if not already started).
+            childProcess.execFileSync(flow, ['status']);
+        } catch (e) {
+            // Probably a linting error, just ignore it for now.
+            // @todo Handle this error properly.
+        }
+    }
+
+    lint(data, path) {
+        return new Promise(function (resolve, reject) {
+            childProcess.execFile(flow, ['status', '--json', '--strip-root'], function (err, stdout, stderr) {
+                const formattedErrors = formatErrors(path, stdout);
+                if (formattedErrors === '') {
+                    resolve();
+                } else {
+                    reject('Flow reported:\n' + formattedErrors);
+                }
+            });
+        });
+    }
+
+}
+
+FlowLinter.prototype.brunchPlugin = true;
+FlowLinter.prototype.type         = 'javascript';
+FlowLinter.prototype.pattern      = /\.jsx?$/;
+
+module.exports = FlowLinter;
