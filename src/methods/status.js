3ccf694dd5e3f1266986f12046ed87be0a41655a
Michael Hogg <michael.hogg.uk@gmail.com>
2016-11-11 18:25:29 +0000

    src/methods/status.js: create main linting function

--- src/methods/status.js
+++ src/methods/status.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// Node.js
+const childProcess = require('child_process');
+
+// NPM
+const flow = require('flow-bin');
+
 // Library
 const errorFormatter = require('../error-formatter.js');
 
@@ -27,3 +33,29 @@ function formatErrors(lintPath, flowStdout) {
     return formattedErrors;
 
 }
+
+/**
+ * Main linting function.
+ *
+ * @param {string}   data
+ * @param {string}   path
+ * @param {Object}   lintingOptions
+ * @param {Function} callback
+ */
+module.exports = function (data, path, lintingOptions, callback) {
+
+    setTimeout(
+        function () {
+
+            const args = ['status', '--json', '--strip-root'];
+
+            childProcess.execFile(flow, args, function (err, stdout, stderr) {
+                const formattedErrors = formatErrors(path, stdout);
+                callback(formattedErrors);
+            });
+
+        },
+        lintingOptions.statusDelay
+    );
+
+};
